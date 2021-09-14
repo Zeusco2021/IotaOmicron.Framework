d51665db1b940f70bb18b5360078143fa02de9f6
Wizard463 <forbidden4u@hotmail.com>
2021-09-14 02:32:49 -0500

    Changed return type in inner documentation for GetSerieById

--- IotaOmicron.Framework.GobMx.Web.API/Controllers/BanxicoController.cs
+++ IotaOmicron.Framework.GobMx.Web.API/Controllers/BanxicoController.cs
@@ -22,7 +22,7 @@ namespace IotaOmicron.Framework.GobMx.Web.API.Controllers
         ///  Creation of the instance that will represent the application that will be managed by this API.
         /// </summary>
         /// <param name="SerieId">The serie identificator to use later on for ask for economic indicators.</param>
-        /// <returns>A JSON Object with the basic data of the serie [Type is BanxicoSeries]</returns>
+        /// <returns>A JSON Object with the basic data of the serie [Type is BanxicoSeriesBmx]</returns>
         [HttpGet("/Banxico/GetBySerieId/{SerieId}")]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(BanxicoSeriesBmx))]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
